7f85ca15019daf6d9de1af09674037be32965a85
GeraldTM <99144289+GeraldTM@users.noreply.github.com>
2023-08-06 18:46:41 -0400

    UI Updates, Config loading

--- main.js
+++ main.js
@@ -3,12 +3,29 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        resizable: false,
         webPreferences: {
         nodeIntegration: true
         },
         icon: __dirname + '/icon.png'
-    })
-    win.loadFile('index.html')
+    });
+    win.loadFile('index.html');
 }
 
-app.whenReady().then(() => createWindow())
+app.on('ready', createWindow);
+
+var CONFIG = require('./modpack.json');
+var slug = CONFIG.modpackSlug;
+var version = CONFIG.modpackVersion;
+
+var minecraft = CONFIG.MCVersion;
+var loader = CONFIG.modloader;
+var loaderVersion = CONFIG.modloaderVersion; 
+
+var theme = CONFIG.theme;
+var primaryColour = CONFIG.primaryColour;
+var secondaryColour = CONFIG.secondaryColour;
+
+function update(){
+    
+}
